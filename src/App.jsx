d52c5d17a72eea9842d84c3079bcbd32be3a2bef
Virgílio César <enjels@outlook.com>
2024-01-30 05:19:41 -0300

    adicionado pequenos ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
 import "./App.css";
 
+//*Hooks
+import useForm from "./hooks/useForm";
+
 function App() {
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+
+  const{currentStep, currentComponent}= useForm(formComponents)
 
   return (
     <div className="app">
@@ -20,7 +25,7 @@ function App() {
       <div className="form-container">
         <p>etapas</p>
         <form>
-          <div className="inputs-container">inputs</div>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             <button type="button">
               <GrFormPrevious />
