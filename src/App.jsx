0c97880c7b36177e40383c423c320bffaf0ed05d
Virgílio César <enjels@outlook.com>
2024-02-01 20:30:42 -0300

    ajustes pequenos no codigo

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import Thanks from "./components/Thanks";
 import "./App.css";
 
 //*Hooks
-import {useForm} from "./hooks/useForm";
+import { useForm } from "./hooks/useForm";
 
 function App() {
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
-  const {currentStep, currentComponent} = useForm(formComponents)
+  const { currentStep, currentComponent, changeStep } = useForm(formComponents);
 
   return (
     <div className="app">
@@ -23,10 +23,10 @@ function App() {
       </div>
       <div className="form-container">
         <p>etapas</p>
-        <form onSubmit={(e)=> changeStep(currentStep + 1, e)}>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type="button" onClick={()=> changeStep(currentStep -1)}>
+            <button type="button" onClick={() => changeStep(currentStep - 1)}>
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
