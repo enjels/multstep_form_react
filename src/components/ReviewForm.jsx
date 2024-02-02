7e0f92de6ad83a525ea058438098ce9a8a2b14dd
Virgílio César <enjels@outlook.com>
2024-02-01 22:38:46 -0300

    adicionado styled do review e amis umas estilizações aparte de outros components tudo funcionando até o momento

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,48 @@
+import {
+  BsFillEmojiFrownFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiHeartEyesFill,
+} from "react-icons/bs";
+
+import "./ReviewForm.css";
+
 const ReviewForm = () => {
   return (
-    <div>
-      <h2>ReviewForm</h2>
+    <div className="review-form">
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" value="unsatisfield" name="review" required />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="neutra" name="review" required />
+          <BsFillEmojiSmileFill />
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="satisfied" name="review" required />
+          <BsFillEmojiNeutralFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="very_satisfied" name="review" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea
+          name="comment"
+          id="comment"
+          placeholder="Conte como foi sau experiência com o produto..."
+          required
+        ></textarea>
+      </div>
     </div>
   );
-}
+};
 
-export default ReviewForm
+export default ReviewForm;
